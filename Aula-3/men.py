48c2e7697c247dc4c376d9d599ba6784ad16ad33
Vicente <atlas001100110@gmail.com>
2026-07-21 19:30:03 -0300

    Add files via upload

---
+++ Aula-3/men.py
@@ -0,0 +1,23 @@
+print("calculador python")
+n1 = 10
+n2 = 5
+soma  =  n1 + n2
+print(soma)
+
+print('subtração')
+n3 = 10
+n4 = 5
+sub= n3-n4
+print (sub)
+
+print('multiplicação')
+n5 = 10
+n6 = 5
+mult = n5 * n6
+print(mult)
+
+print("divisão")
+n7 = 10
+n8 = 5
+div= n7/n8
+print(div)
