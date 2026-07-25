e8f4c1c2374fcaaef29f6300d94b2c2ff9c57b23
Vicente <atlas001100110@gmail.com>
2026-07-24 21:57:11 -0300

    Add files via upload

---
+++ Aula-8/cadastro.py
@@ -0,0 +1,24 @@
+# Sistema de Reservas de Hotel:
+
+cliente_1 = str(input())
+idade_c1 =
+
+
+cliente_2 =
+idade_c2 =
+
+
+cliente_3 =
+idade_c3 =
+
+
+
+
+
+
+
+
+
+print(" Olá seja bem vindo ao Hotel Bristol ")
+print('Tenha um otimo dia')
+
