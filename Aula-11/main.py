35bbb4dc6e85a4b2fb228761a35e30bee4979a28
Vicente <atlas001100110@gmail.com>
2026-07-30 21:52:57 -0300

    Add files via upload

---
+++ Aula-11/main.py
@@ -0,0 +1,12 @@
+try:
+ n1 = float(input("digite seu primeiro numero"))
+ n2 = float(input("diite mais um.... "))
+
+ resultado = n1/n2
+
+ print(resultado)
+
+except: ZeroDivisionError
+print (" erro não divida por 0 em py..... >:( ")
+
+
