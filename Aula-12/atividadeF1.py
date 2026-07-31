22f5402df16b07b219bbd40ff28fbf04f95ef897
Vicente <atlas001100110@gmail.com>
2026-07-30 21:53:22 -0300

    Add files via upload

---
+++ Aula-12/atividadeF1.py
@@ -0,0 +1,37 @@
+
+print("bem vindo...")
+print("me fale numeros direi se ele é par ou impar !!!!")
+
+
+def CPR():
+    n1 = int(input("me fale o primeiro numero"))
+
+    print("___________________________________________")
+
+    n2= int(input("me fale o segundo numero"))
+
+    print("ok......")
+    print("um instante.....")
+
+    if n1 % 2==0:
+        resultado1= "par"
+    else:
+        resultado1= "impar"
+
+        
+
+
+        if n2 % 2==0:
+
+            resultado2 = "par"
+            
+        else:
+            resultado2 = "impar"
+
+
+
+CPR()
+ 
+
+
+
---
+++ Aula-12/atividadeF2.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+def mult():
+
+ n1= input("me fale 1 numero")
+
+ n2 = input("me fale o seundo numero")
+
+ n3 = input("me fale o terceiro numero")
+
+ soma = n1*n2*n3
+
+ print (soma)
+
+ mult()
