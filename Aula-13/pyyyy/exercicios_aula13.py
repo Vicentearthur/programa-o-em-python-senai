70b95caa074069d590e65d564b9c6d7601e038e3
Vicente <atlas001100110@gmail.com>
2026-08-04 21:56:17 -0300

    Add files via upload

---
+++ Aula-13/pyyyy/exercicios_aula13.py
@@ -0,0 +1,13 @@
+
+import modulo
+
+
+print(modulo.atv_1())
+
+print(modulo.atv_2())
+
+print(modulo.atv_3())
+
+modulo.atv_4()
+
+modulo.atv_5()
---
+++ Aula-13/pyyyy/modulo.py
@@ -0,0 +1,27 @@
+import random
+
+
+def atv_1():
+    n = random.randint(5,10)
+    return n
+
+
+
+def atv_2():
+    n1 = random.randint(5,10)
+    n2 = random.randint(5,10)
+    n3 = random.randint(5,10)
+    return n1,n2,n3
+    
+
+def atv_3():
+    n = random.randint(10,30)
+    return n
+
+def atv_4():
+    for contagem in range(10,0,-1):
+        print(contagem)
+    print('fogo!')    
+
+def atv_6():
+    numero = int(input("digite um numero de 0 a 10"))
